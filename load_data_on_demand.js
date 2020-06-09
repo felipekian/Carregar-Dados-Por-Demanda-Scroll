1f4683a208180b4af166cf45130a4ee0b6f7c4d5
Felipe Derkian <felipekian@yahoo.com.br>
2020-06-09 13:16:01 -0300

    source code

---
+++ load_data_on_demand.js
@@ -0,0 +1,26 @@
+cont_scroll_page_end = 1
+
+function getProxDatas() {
+    $.ajax({
+        type: '',
+        url: '',
+        data: {
+            'scroll_count': cont_scroll_page_end
+        },
+        dataType: 'json',
+        success: function (response) {
+            if (response.status == 1) {
+                
+            } else {
+                
+            }
+        }
+    });
+}
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() + $(this).height() >= $(document).height() * 0.9) {
+        cont_scroll_page_end++;
+        getProxDatas();
+    }
+});
